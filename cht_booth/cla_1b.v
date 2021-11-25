5b20b29325c63e350c3d2800b51c7637bbff1b11
cstandy <cst.andy@gmail.com>
2021-11-25 11:44:19 -0500

    Feat: Add gate-level cla_1b

--- cht_booth/cla_1b.v
+++ cht_booth/cla_1b.v
@@ -1,14 +1,29 @@
 module cla_1b(s, cin, p, g, a, b);
 
 	input a, b, cin;
-	output reg s;
-    output reg p;
-    output reg g;
+
+	output s;
+    output p;
+    output g;
+
+    wire p;
+    wire g;
+    wire and_pc;
+
+    xor(p, a, b);
+    xor(s, p, cin);
+    and(and_pc, p, cin);
+    and(g, a, b);
+    or(cout, g, and_pc);
+
+	// output reg s;
+    // output reg p;
+    // output reg g;
 	
-    always @(*) begin
-        p = a ^ b;
-    	s = p ^ cin;
-        g = a & b;
-    end
+    // always @(*) begin
+    //     p = a ^ b;
+    // 	s = p ^ cin;
+    //     g = a & b;
+    // end
 
 endmodule
